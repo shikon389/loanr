b12858ac9ab4ad374ada801fe12d0bd21325af4a
kapadiamush <kapadiamush@gmail.com>
2015-04-18 18:25:09 -0400

    models

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var User = require('./models/user');
+var Loan = require('./models/loan');
+
 var router = express.Router();
 
 /* root route */
 router.get('/', function(req, res) { 
     res.json({
         'status' : true
+    });
+});
+
+router.get('/:username/:asdf', function(req, res){
+    res.json({
+        'status' : true, 
+        'message' : req.params.username + " " + req.params.asdf
+
     });
 });
 
