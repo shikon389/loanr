38eb022c35eefdc27d4b15d38c73fba94344466a
kapadiamush <kapadiamush@gmail.com>
2015-04-18 20:08:19 -0400

    updated models adding authentication

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var db = require('./configs/db');
+var passport = require('./configs/passport');
 
 var app = express();
 
