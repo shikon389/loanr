f74a5d6e3022fd91d06178d99fc44eacd38b095e
kapadiamush <kapadiamush@gmail.com>
2015-04-19 00:35:00 -0400

    requesting venom account

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -48,9 +48,13 @@ router.get('/:username', function(req, res){
 
         var reqUrl = "https://api.venmo.com/v1/users/" + user.venmoId + "?access_token=" + user.access_token;
 
-        request.get(reqUrl).on('response', function(response){
-            console.log(response);  
-            user.venmo = response;
+        request({
+            uri: reqUrl,
+            method: "GET",
+        },
+        function(err, response, body){
+            console.log(body); 
+            user.venmo_account = body; 
             return res.json({
                 'status': true, 
                 'user': user
