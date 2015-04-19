31f240758f41d3f0c6ce76a1b874e891367bd696
shikon389 <siconninja@gmail.com>
2015-04-18 20:08:05 -0400

    added loan get requests

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var User = require('../models/user');
 var Loan = require('../models/loan');
-
+var request = require('request');
 var router = express.Router();
 
 router.get('/', function(req, res){
@@ -27,6 +27,8 @@ router.get('/:username', function(req, res){
             });
         }
 
+
+
         return res.json({
             'status': true, 
             'user': user
@@ -34,5 +36,40 @@ router.get('/:username', function(req, res){
     });
 });
 
+router.get('/:username/:loanID', function(req, res){
+    User.findOne({'username' : req.params.username}, function(err, user){
+        if(err){
+            return res.json({
+                'status': false, 
+                'error': err
+            });
+        }
+
+        if(!user){
+            return res.json({
+                'status': false, 
+                'message': "Couldn't find loan"
+            });
+        }
+
+        for(var i = 0; i < user.loans_given.length(); i++){
+            if(user.loans_given[i] == 'loanID'){
+                Loan.findOne({'_id' : req.params.loanID}, function(err, loan){
+                    if(err){
+                        return res.json({
+                            'status': false, 
+                            'error': err
+                        });
+                    }
+
+                    return res.json({
+                        'status': true, 
+                        'loan': loan;
+                    });    
+                });
+            }
+        }
+    });
+});
 
 module.exports = router;
