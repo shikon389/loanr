0ddc10f57ac5e3e536cb6594e287beee3759b09b
kapadiamush <kapadiamush@gmail.com>
2015-04-18 23:19:28 -0400

    save the dam user doe

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res){
 /* User Autherization */
 router.get('/authenticate', passport.authenticate('venmo', {scope: ['make_payments', 'access_balance', 'access_friends' ]}));
 
-router.get('/authenticate/callback', function(req, res){
+router.get('/authenticate/callback', passport.authenticate('venmo'), function(req, res){
     if(req.user){
         return res.json({
             "status" : true
