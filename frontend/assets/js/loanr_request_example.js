8ab6ef5f820bf6b6e18799c9441514d22595cfdd
Neil Patel <neil1023@yahoo.com>
2015-04-19 03:18:50 -0400

    added http status checker within javascript

--- frontend/assets/js/loanr_request_example.js
+++ frontend/assets/js/loanr_request_example.js
@@ -2,20 +2,29 @@ var request = new XMLHttpRequest();
 
 request.open("GET", "http://loanr.thenoobprogrammer.com/api/users/currentUser", true);
 
-request.onreadystatechange = function(){
-    response = JSON.parse(request.responseText);
+request.onreadystatechange = acquireUser;
 
-    var req2Url = "http://loanr.thenoobprogrammer.com/api/users/" + response.user.username; 
+function acquireUser(){
+	 if (xmlHttp.readyState == 4) {
+        if (xmlHttp.status == 200) {
+		    response = JSON.parse(request.responseText);
 
-    var req2 = new XMLHttpRequest(); 
-    req2.open("GET", req2Url, true); 
+		    var req2Url = "http://loanr.thenoobprogrammer.com/api/users/" + response.user.username; 
 
-    req2.onreadystatechange = function(){
-        $('#name').innerHTML = xmlHttp.responseText;
-    };
+		    var req2 = new XMLHttpRequest(); 
+		    req2.open("GET", req2Url, true); 
 
-    req2.send();
+		    req2.onreadystatechange = function(){
+		    	if (xmlHttp.readyState == 4) {
+        			if (xmlHttp.status == 200) {
+		        		$('#name').innerHTML = xmlHttp.responseText;
+		        	}
+		        }
+		    };
 
+		    req2.send();
+		}
+	}
 }; 
 
 request.send();
